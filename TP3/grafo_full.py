d2874a257a28cda075c01f13dbafd02d0249d8fb
franzimbi <fzimbimbakis@fi.uba.ar>
2021-12-12 14:33:58 -0300

    tda corregido por MB

--- TP3/grafo_full.py
+++ TP3/grafo_full.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
+import random
+from collections import deque
 
-class Nodo:
-    def __init__(self, clave, peso):
-        self._clave = clave
-        self._peso = peso
 
-    def peso(self):
-        return self._peso
-
-    def clave(self):
-        return self._clave
-
-    def __str__(self):
-        res ='clave: ' + str(self._clave)
-        if(self._peso != None):
-            res+= '->peso: ' + str(self._peso)
-        return res
-
-    
 class Grafo:
     def __init__(self, dirigido=False):
         self._dirigido = dirigido
@@ -32,51 +17,58 @@ class Grafo:
 
     """inserta una arista con peso. en caso de no ser pesado no insertar peso. en caso de no ser dirigido, origen y destino son indistintos"""
     def insertar_arista(self, origen, destino, peso=None):
-        if origen and destino in self._vertices:
+        if origen in self._vertices and destino in self._vertices:
             self._vertices[origen][destino] = peso
             if self._dirigido == False:
                 self._vertices[destino][origen] = peso
+            return True
+        return False
 
+    """devuelve una lista con los vertices adyacentes"""
     def adyacentes(self, vertice):
         if vertice in self._vertices:
-            return set(self._vertices[vertice].keys())
+            return [self._vertices[vertice].keys()]
+        return None
 
+    """saca un vertice y devuelve su dato"""
     def sacar_vertice(self, vertice):
         for i in self._vertices:
             self._vertices[i].pop(vertice)
         self._vertices.pop(vertice)
-        self._datos.pop(vertice)
+        return self._datos.pop(vertice)
 
-
-    def sacar_vertice(self, origen, destino):
+    """saca una arista"""
+    def sacar_arista(self, origen, destino):
         self._vertices[origen].pop(destino)
         if self._dirigido == False:
             self._vertices[destino].pop(origen)
 
+    """devuelve True si dos vertices estan unidos por una arista. False en caso contrario"""
     def ver_dos_vertices_unidos(self, origen, destino):
-        if destino in self._vertices[origen]:
-            return True
+        if origen in self._vertices:
+            return destino in self._vertices[origen]
         return False
 
+    """devuelve True si un vertice existe. False en caso contrario"""
     def existe_vertice(self, vertice):
-        if vertice in self._vertices:
-            return True
-        return False
-
+        return vertice in self._vertices
+    
+    """devuelve el dato del vertice"""
     def dato_vertice(self, vertice):
         if vertice in self._vertices:
             return self._datos[vertice]
         return None
 
+    """devuelve una lista con todos los vertices"""
     def todos_vertices(self):
-        return [self._vertices.keys()]
+        return list(self._vertices.keys())
+
+    """devuelve un vertice aleatorio"""
+    def vertice_aleatorio(self):
+        return random.choice(list(self._vertices.keys())) 
 
     def __iter__(self):
-        self._iter = iter(self._vertices)
-        return self._iter
-
-    def __next__(self):
-        return next(self._iter)
+        return iter(self._vertices)
 
     def __str__(self):
         res = ''
@@ -86,28 +78,48 @@ class Grafo:
                 res += str(self._vertices[i].copy()) + '\n'
             else:
                 res += '\n'
-
         return res
 
-no_dirigido = Grafo(False)
+def bfs(grafo, origen):
+    visitados = set()
+    padres = {}
+    orden = {}
+    padres[origen] = None
+    orden[origen] = 0
+    visitados.add(origen)
+    q = deque()
+    q.append(origen) #encolar
+    while len(q) != 0:
+        v = q.popleft() #desencolar
+        for w in grafo.adyacentes(v):
+            if w not in visitados:
+                padres[w] = v
+                orden[w] = orden[v] + 1
+                visitados.add(w)
+                q.append(w)
+    return padres, orden
 
-no_dirigido.insertar_vertice('a', 2)
-no_dirigido.insertar_vertice('b', 3)
-no_dirigido.insertar_vertice('c', 4)
-no_dirigido.insertar_vertice('d', 5)
-no_dirigido.insertar_vertice('e', 6)
-no_dirigido.insertar_vertice('f', 7)
 
-no_dirigido.insertar_arista('a', 'b', 2)
-no_dirigido.insertar_arista('a', 'c', 4)
-no_dirigido.insertar_arista('a', 'f', 6)
-no_dirigido.insertar_arista('d', 'b', 4)
-no_dirigido.insertar_arista('e', 'b', 6)
-no_dirigido.insertar_arista('c', 'f', -12)
+grafo = Grafo()
 
+grafo.insertar_vertice('a')
+grafo.insertar_vertice('b')
+grafo.insertar_vertice('c')
+grafo.insertar_vertice('d')
+grafo.insertar_vertice('e')
+grafo.insertar_vertice('f')
+grafo.insertar_vertice('g')
+grafo.insertar_vertice('h')
+grafo.insertar_vertice('i')
 
+grafo.insertar_arista('a', 'b')
+grafo.insertar_arista('a', 'c')
+grafo.insertar_arista('c', 'd')
+grafo.insertar_arista('e', 'd')
+grafo.insertar_arista('g', 'h')
+grafo.insertar_arista('i', 'b')
+grafo.insertar_arista('c', 'e')
 
-for i in no_dirigido:
-    print(no_dirigido.sacar_vertice(i))
+print(grafo)
 
-    
+print(bfs(grafo, 'a'))
